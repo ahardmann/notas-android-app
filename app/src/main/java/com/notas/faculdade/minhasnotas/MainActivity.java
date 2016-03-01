911cf94401f1148a9cc60c130c312d096cd87fd9
Arthur Hage <arthur.hage@C305-ADS-036.fieb.org.br>
2016-03-01 17:59:28 -0300

    update files

--- app/src/main/java/com/notas/faculdade/minhasnotas/MainActivity.java
+++ app/src/main/java/com/notas/faculdade/minhasnotas/MainActivity.java
@@ -2,13 +2,13 @@ package com.notas.faculdade.minhasnotas;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.notas.faculdade.minhasnotas.materias.MateriasListActivity;
+import com.notas.faculdade.minhasnotas.materias.CadMateriaActivity;
 
 public class MainActivity extends AppCompatActivity {
 
